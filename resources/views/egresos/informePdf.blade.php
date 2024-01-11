fa96ef2cf37f9488031788a126977e2ec9a9ca65
vadelgado <vadelgado@udenar.edu.co>
2024-01-11 18:26:51 -0500

    Suma al total Egresos

--- resources/views/egresos/informePdf.blade.php
+++ resources/views/egresos/informePdf.blade.php
@@ -30,35 +30,39 @@
 
     @if(count($egresos) > 0)
     <table id="egresos" class="table table-striped table-bordered" style="width:100%">
-                        <thead style="background-color: #f8f9fa;">
-                            <tr>
-                                <th>#</th>
-                                <th>Tipo</th>
-                                <th>Descripción</th>
-                                <th>Fecha y Hora</th>
-                                <th>Realizó</th>
-                                <th>Recibió</th>
-                                <th>Monto</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tableBody">
-                            @forelse ($egresos as $i => $s)
-                                <tr style="background-color: #fff;" onmouseover="this.style.backgroundColor='#f8f9fa';" onmouseout="this.style.backgroundColor='#fff';">
-                                    <td>{{ $i + 1 }}</td>
-                                    <td>{{ $s->nombre_tipo }}</td>
-                                    <td>{{ $s->descripcion_egreso }}</td>
-                                    <td>{{ $s->fecha_hora }}</td>
-                                    <td>{{ $s->name }}</td>
-                                    <td>{{ $s->a_quien_se_dio }}</td>
-                                    <td>{{ $s->valor }}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="5">No hay egresos disponibles</td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+        <thead style="background-color: #f8f9fa;">
+            <tr>
+                <th>#</th>
+                <th>Tipo</th>
+                <th>Descripción</th>
+                <th>Fecha y Hora</th>
+                <th>Realizó</th>
+                <th>Recibió</th>
+                <th>Monto</th>
+            </tr>
+        </thead>
+        <tbody id="tableBody">
+            @forelse ($egresos as $i => $s)
+                <tr style="background-color: #fff;" onmouseover="this.style.backgroundColor='#f8f9fa';" onmouseout="this.style.backgroundColor='#fff';">
+                    <td>{{ $i + 1 }}</td>
+                    <td>{{ $s->nombre_tipo }}</td>
+                    <td>{{ $s->descripcion_egreso }}</td>
+                    <td>{{ $s->fecha_hora }}</td>
+                    <td>{{ $s->name }}</td>
+                    <td>{{ $s->a_quien_se_dio }}</td>
+                    <td>{{ $s->valor }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="7">No hay egresos disponibles</td>
+                </tr>
+            @endforelse
+            <tr style="background-color: #f2f2f2;">
+                <td colspan="6" style="text-align: right;"><strong>Total:</strong></td>
+                <td>{{ $egresos->sum('valor') }}</td>
+            </tr>
+        </tbody>
+    </table>
     @else
         <p>No se encontraron datos para el informe.</p>
     @endif
